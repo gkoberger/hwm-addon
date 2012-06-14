3b76c85d52abcdd2461ab21c5860321a064f6921
Gregory Koberger <gkoberger@gmail.com>
2012-06-14 15:52:57 -0700

    Add class to body if installed

--- src/check.js
+++ src/check.js
@@ -7,3 +7,5 @@ if(el) {
     var vals = location.href.match(/([a-zA-Z0-9]*)-([0-9]*)/);
     window.location.href = "http://hulu.com/watch/" + vals[2] + "/#hwm-" + vals[1];
 }
+
+document.getElementsByTagName('body')[0].className = "is_installed";
